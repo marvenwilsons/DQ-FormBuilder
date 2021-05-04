1d067b5a9ec18355f23c868d42d22ea9f07e5d1a
Marven <marvenwilsons@gmail.com>
2021-05-04 16:42:56 -0400

    input range done

--- components/range-slider.vue
+++ components/range-slider.vue
@@ -1,5 +1,5 @@
 <template>
-    <main style="padding:0;" class="flex flexcol borderRad4 relative overflowhidden">
+    <main style="padding:0;" class="flex flexcol borderRad4 relative">
         <div style="border:1px solid gray;" class="pad050 flex spacebetween borderRad4 flexcenter" >
             <div>
                 <span style="color: gray" >{{label}}:</span> 
@@ -9,24 +9,27 @@
         </div>
         <div 
             v-if="showSlider"
-            :style="{background: 'whitesmoke'}" 
-            class="relative flex pad050 " 
+            :style="{background: 'white', boxShadow: '-1px 4px 9px 0px rgba(187,187,187,1)', top: '100%', zIndex:'100'}" 
+            class="absolute flex pad050 fullwidth borderRad4"
+            id="rsp" 
         >
-            <span class="marginright025" >
-                <small>{{min}}</small>
-            </span>
-            <input
-                style="z-index: 2" 
-                v-model="currentValue" 
-                id="rangeSlider" 
-                class="margintop050 marginbottom025 fullwidth" 
-                type="range" 
-                :min="min" 
-                :max="max"
-            >
-            <span class="marginleft025" >
-                <small>{{max}}</small>
-            </span>
+            <div class="fullwidth flex pad025" >
+                <span class="marginright025" >
+                    <small>{{min}}</small>
+                </span>
+                <input
+                    style="z-index: 2" 
+                    v-model="currentValue" 
+                    id="rangeSlider" 
+                    class="margintop050 marginbottom025 fullwidth" 
+                    type="range" 
+                    :min="min" 
+                    :max="max"
+                >
+                <span class="marginleft025" >
+                    <small>{{max}}</small>
+                </span>
+            </div>
         </div>
     </main>
 </template>
@@ -78,5 +81,10 @@ export default {
   border: 1px solid #5a8ab9;
   border-radius: 999px;
   background: #d3d3d334; 
+}
+#rsp{
+    -webkit-box-shadow: 0px 0px 68px -28px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 0px 68px -28px rgba(0,0,0,0.75);
+    box-shadow: 0px 0px 68px -28px rgba(0,0,0,0.75);
 }
 </style>
